3393c22d1c767c6bb27e1feb3843bd9dd08c3c33
Elaidzha1940 <eli080293@gmail.com>
2023-04-21 02:01:20 +0700

    CustomSegmentedPicker

---
+++ CustomSegmentedPicker/CustomSegmentedPicker/CustomSegmentedPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSegmentedPickerApp.swift
+//  CustomSegmentedPicker
+//
+//  Created by Elaidzha Shchukin on 21.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomSegmentedPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
